93ab657c8d7644652066270467fafbd89d2e1f07
dipta harimbawa <vipalkuda@gmail.com>
2022-12-05 02:40:59 +0700

    [add] infinite scroll

--- resources/js/Pages/Category.vue
+++ resources/js/Pages/Category.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
+<div ref="scrollComponent">
     <!-- ...:::Start Search & Filter Section:::... -->
-    <Search v-model:search-text="searchText" v-model:slider-default="defaultValue" v-model:slider-max="maxValueSlider"/>
+    <Search v-model:search-text="searchText" v-model:slider-default="defaultValue" v-model:slider-max="maxValueSlider" />
     <!-- ...:::End Search & Filter Section:::... -->
 
     <!-- ...:::Start Catagories - 1 Section:::... -->
@@ -9,17 +9,15 @@
         <div class="container">
             <div class="catagories-area">
                 <div class="catagories-nav-1">
-                    <CategoryLabel :categories="categories"/>
+                    <CategoryLabel :categories="categories" />
                 </div>
             </div>
 
             <div class="catagories-wrapper catagories-shop-wrapper">
 
                 <div class="catagories-wrapper-content">
-                    <product @update-wishlist-click="getWishlistLocal" v-for="product in product.data" :key="product.id" :product="product" :wishlistLocal="currentWishlist" />
+                    <product @update-wishlist-click="getWishlistLocal" v-for="product in productList" :key="product.id" :product="product" :wishlistLocal="currentWishlist" />
                 </div>
-
-                <button class="load-more-btn">load more</button>
             </div>
         </div>
     </div>
@@ -34,12 +32,19 @@ export default {
 }
 </script>
 <script setup>
-import { onMounted, ref, watch } from '@vue/runtime-core'
+import {
+    onMounted,
+    onUnmounted,
+    ref,
+    watch
+} from '@vue/runtime-core'
 import CategoryLabel from "../Shared/Components/Home/CategoryLabel.vue"
 import Product from "../Shared/Components/Home/Product.vue"
 import Search from "../Shared/Components/Category/Search.vue"
 import debounce from 'lodash/debounce'
-import { Inertia } from '@inertiajs/inertia'
+import {
+    Inertia
+} from '@inertiajs/inertia'
 const props = defineProps({
     product: Object,
     categories: Object,
@@ -51,7 +56,14 @@ const props = defineProps({
 // Memanggil fungsi saat mount
 onMounted(() => {
     getWishlistLocal()
+    window.addEventListener("scroll", handleScroll)
 })
+onUnmounted(() => {
+    window.removeEventListener("scroll", handleScroll)
+})
+
+// variable untuk menampung product
+const productList = ref(props.product.data)
 
 let maxValueSlider = props.maxPrice
 
@@ -64,14 +76,39 @@ let searchText = ref(props.filters.search)
 // Deklarasikan variable penampungan wishlist
 let currentWishlist = ref([]);
 
+ const scrollComponent = ref(null)
+
 const getWishlistLocal = () => {
     // mengambil data wishlist dari localstorage
     let wishlistLocal = localStorage.getItem('wishlist');
     currentWishlist.value = wishlistLocal != 'undefined' && wishlistLocal != null ? JSON.parse(wishlistLocal) : []
 }
 
+const handleScroll = (e) => {
+    let element = scrollComponent.value
+  if (element.getBoundingClientRect().bottom < window.innerHeight) {
+    addProductOnScroll()
+  }
+}
+
+const addProductOnScroll = () => {
+    if (props.product.next_page_url === null) {
+        return;
+    }
+
+    Inertia.get(props.product.next_page_url, {}, {
+        preserveScroll: true,
+        preserveState: true,
+        replace: true,
+        only: ['product'],
+        onSuccess: () => {
+            productList.value = [...productList.value, ...props.product.data]
+        }
+    })
+}
+
 // Melakukan query baru pencarian product ketika user menuliskan nama barang / memilih filter harga
-watch([searchText, defaultValue], debounce(function ([newSearch, newDefaultValue],[oldSearch, oldDefaultValue]) {
+watch([searchText, defaultValue], debounce(function ([newSearch, newDefaultValue], [oldSearch, oldDefaultValue]) {
     Inertia.get('/category/' + props.categoryId, {
         search: newSearch,
         min: newDefaultValue[0],
