73e3df2af1a31413f4ec861deb50d09e89dd0bc6
dipta harimbawa <vipalkuda@gmail.com>
2023-01-20 00:43:40 +0700

    [fix] login route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,8 @@
             <h1 class="auth-title">Log in.</h1>
             <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
 
-            <form action="index.html">
+            <form action="{{route('auth.index')}}" method="POST">
+                @csrf
                 <div class="form-group position-relative has-icon-left mb-4">
                     <input type="text" name="username" class="form-control form-control-xl" placeholder="Username">
                     <div class="form-control-icon">
