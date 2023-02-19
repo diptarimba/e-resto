c0f7785bb20abca6ace633c7fcef89e2a739a346
dipta harimbawa <vipalkuda@gmail.com>
2023-02-20 01:51:03 +0700

    [add] home

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\Order;
+use App\Models\Payment;
+use App\Models\Product;
+use App\Models\Table;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,6 +15,11 @@ class HomeController extends Controller
     public function index()
     {
         $sales = Order::count();
-        return view('admin.home.index', compact('sales'));
+        $customer = Customer::count();
+        $product = Product::count();
+        $table = Table::count();
+        $payment = Payment::count();
+
+        return view('admin.home.index', compact('sales', 'customer', 'product', 'table', 'payment'));
     }
 }
--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -11,11 +11,15 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body text-center">
-                <span class="h4">Statistik Penjualan</span>
+                <span class="h4">Statistik (Total)</span>
             </div>
         </div>
     </div>
-    <x-card.card-statistic text="Total Penjualan" value="{{$sales}}"/>
+    <x-card.card-statistic text="Penjualan" value="{{$sales}}" unit="Transaksi"/>
+    <x-card.card-statistic text="Customer" value="{{$customer}}" unit="Orang"/>
+    <x-card.card-statistic text="Produk" value="{{$product}}" unit="Unit"/>
+    <x-card.card-statistic text="Meja" value="{{$table}}" unit="Unit"/>
+    <x-card.card-statistic text="Metode Pembayaran" value="{{$payment}}" unit="Metode"/>
 </x-layoutDashboard>
 @endsection
 
