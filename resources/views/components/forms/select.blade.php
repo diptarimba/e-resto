0d92ff27e8357eb41c36602087d2f851097a4e1f
dipta harimbawa <vipalkuda@gmail.com>
2023-03-25 21:45:13 +0700

    [remove] default value

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -1,7 +1,6 @@
 <div class="mb-2">
     <div  class="col-form-label">{{ $label }}</div>
     <select class="form-control" {{ $multiple }} data-placeholder="{{ $placeholder }}" name="{{ $name }}">
-        <option>Default Value</option>
         @foreach ($items as $id => $name)
             <option value="{{ $id }}" @if (@$value == $id) selected @endif>{{ $name }}</option>
         @endforeach
