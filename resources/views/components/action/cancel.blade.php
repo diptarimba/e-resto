4dba8c3592324b18b8d3c76bb1a166d96797eac6
dipta harimbawa <vipalkuda@gmail.com>
2023-03-30 05:07:22 +0700

    [fix] cancel

--- resources/views/components/action/cancel.blade.php
+++ resources/views/components/action/cancel.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ url()->previous() }}" class="mx-1 my-1 btn btn-outline-warning btn-pill">Back</a>
+<a href="{{ $href ?? url()->previous() }}" class="mx-1 my-1 btn btn-outline-warning btn-pill">Back</a>
