ddf245a4e4ad5735fb9cca45457d1f63758a6318
dipta harimbawa <vipalkuda@gmail.com>
2023-04-02 03:38:11 +0700

    [fix] footer

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -31,7 +31,7 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2022 &copy; {{env('APP_NAME')}}</p>
+                        <p>{{date("Y")}} &copy; {{env('APP_NAME')}}</p>
                     </div>
                     <div class="float-end">
                         {{-- <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
