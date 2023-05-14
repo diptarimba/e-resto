8298372ed1f51944a89f9cd3588ff3d5c325306e
khafidhasukma@gmail.com <khafidhasukma@gmail.com>
2023-05-14 08:36:36 +0700

    [auth/login]: fix style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,53 +13,50 @@
 
 <body>
     <div id="auth">
+        <div class="row h-100">
+            <div class="col-md-6 col-xl-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="index.html"><img src="{{ asset('dashboard-assets//images/logo/logo.svg')}}" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title">Log in.</h1>
+                    <p class="auth-subtitle mb-4">Log in with your data that you entered during registration.</p>
 
-<div class="row h-100">
-    <div class="col-lg-5 col-12">
-        <div id="auth-left">
-            <div class="auth-logo">
-                <a href="index.html"><img src="{{ asset('dashboard-assets//images/logo/logo.svg')}}" alt="Logo"></a>
-            </div>
-            <h1 class="auth-title">Log in.</h1>
-            <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
-
-            <form action="{{route('auth.post')}}" method="POST">
-                @csrf
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" name="username" class="form-control form-control-xl" placeholder="Username">
-                    <div class="form-control-icon">
-                        <i class="bi bi-person"></i>
+                    <form action="{{route('auth.post')}}" method="POST">
+                        @csrf
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" name="username" class="form-control form-control-xl" placeholder="Username">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" name="password" class="form-control form-control-xl" placeholder="Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <div class="form-check form-check-lg d-flex align-items-center gap-2">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                                Keep me logged in
+                            </label>
+                        </div>
+                        <button class="btn btn-primary btn-block font-weight-bold btn-lg shadow-lg mt-5">Log in</button>
+                    </form>
+                    <div class="text-center mt-5">
+                        <p class="text-gray-600 mb-1">Don't have an account? <a href="auth-register.html" class="font-bold">Sign
+                                up</a>.</p>
+                        <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a></p>
                     </div>
                 </div>
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" name="password" class="form-control form-control-xl" placeholder="Password">
-                    <div class="form-control-icon">
-                        <i class="bi bi-shield-lock"></i>
-                    </div>
+            </div>
+            <div class="col-md-6 col-xl-7 d-none d-md-block">
+                <div id="auth-right">
+
                 </div>
-                <div class="form-check form-check-lg d-flex align-items-end">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                        Keep me logged in
-                    </label>
-                </div>
-                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-            </form>
-            <div class="text-center mt-5 text-lg fs-4">
-                <p class="text-gray-600">Don't have an account? <a href="auth-register.html" class="font-bold">Sign
-                        up</a>.</p>
-                <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
             </div>
         </div>
-    </div>
-    <div class="col-lg-7 d-none d-lg-block">
-        <div id="auth-right">
-
-        </div>
-    </div>
-</div>
-
     </div>
 </body>
-
 </html>
