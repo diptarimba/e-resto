bb71c4fea83de786aba4813651e761b032f848d3
khafidhasukma@gmail.com <khafidhasukma@gmail.com>
2023-06-01 16:33:26 +0700

    [remove] image sneakers

--- resources/js/Shared/Components/Home/CategoryLabel.vue
+++ resources/js/Shared/Components/Home/CategoryLabel.vue
@@ -1,13 +1,17 @@
 <template>
-<swiper slides-per-view="auto" :space-between="12">
-    <swiper-slide v-for="eachCateory in categories" :key="eachCateory.id">
-        <Link :href="route('category.expand', eachCateory.id, false)" class="btn"><span class="icon"><img width="33" height="33" src="/assets/images/catagories/sneakers.png" alt="image"></span> {{eachCateory.name}}</Link>
-    </swiper-slide>
-</swiper>
+    <swiper slides-per-view="auto" :space-between="12">
+        <swiper-slide v-for="eachCateory in categories" :key="eachCateory.id">
+            <Link
+                :href="route('category.expand', eachCateory.id, false)"
+                class="btn"
+                ><span class="icon"></span> {{ eachCateory.name }}</Link
+            >
+        </swiper-slide>
+    </swiper>
 </template>
 <script setup>
-import 'swiper/css';
+import "swiper/css";
 defineProps({
-    categories: Object
-})
+    categories: Object,
+});
 </script>
